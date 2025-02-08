269590a177c08f1ff1c317db2ae147fb69f1b520
Antriksh Prasad <antrikshprasad@Antrikshs-MacBook-Air.local>
2025-02-08 19:03:46 +0530

    add comment

--- cruddemo/src/main/java/com/demo/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo/src/main/java/com/demo/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.demo.springboot.cruddemo.dao;
 import com.demo.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// we have passed the Entity Type and Primary Key, so spring will give crud implementation
+// no need for DAO impl class
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
